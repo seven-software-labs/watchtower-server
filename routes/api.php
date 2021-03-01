ba13dede50f4f46bfc06df134448f9a29e84284e
Jonathan Tordesillas <jonathan.tordesillas@5align.com>
2021-03-01 19:31:32 +0800

    Add first api resource.

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => []], function() {
+    Route::apiResources([
+        'statuses' => \App\Http\Controllers\StatusController::class,
+    ]);
+});
