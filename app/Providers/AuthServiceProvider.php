fa28b8d7a458708dfff7203aab95eead468c0477
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-02-07 14:53:14 +0000

    Upgrade to Laravel 10.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -27,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         if (! $this->app->routesAreCached()) {
-            Passport::routes();
+            // ...
         }
     }
 }
