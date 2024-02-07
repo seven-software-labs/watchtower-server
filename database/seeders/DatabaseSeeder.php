40d4fde1ed6ebaedef977caa48f22b39a2df3a8d
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-02-07 13:43:44 +0000

    Replace create with firstOrCreate to prevent duplicates and fix typo.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,14 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $organization = Organization::create([
+        $organization = Organization::firstOrCreate([
             'name' => 'Watchtower Support',
-            'subdomain' => 'eatchtowersupport',
+            'subdomain' => 'watchtowersupport',
+        ], [
+            // ...
         ]);
 
-        $user = User::create([
-            'name' => 'Watchtower Support',
+        $user = User::firstOrCreate([
             'email' => 'support@watchtowersupport.com',
+        ], [
+            'name' => 'Watchtower Support',
             'password' => Hash::make('password'),
             'master_organization_id' => $organization->getKey(),
             'email_verified_at' => now(),
